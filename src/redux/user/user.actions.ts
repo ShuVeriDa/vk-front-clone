bd0b98f65086341b3fb4bf1974f333fea28c5ce9
ShuVeriDa <bashtarov@outlook.com>
2023-03-23 09:28:14 +0300

    added the ability to request login, logout, checkAuth

--- src/redux/user/user.actions.ts
+++ src/redux/user/user.actions.ts
@@ -1,12 +1,43 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import {AuthService} from "../../services/auth/auth.service";
-import {IAuthResponse, IRegister} from "./user.interface";
+import {IAuthResponse, ILogin, IRegister} from "./user.interface";
+import {errorCatch} from "../../api/api.helper";
 
 export const registerTC = createAsyncThunk<IAuthResponse, IRegister>('auth/register', async (data, thunkAPI) => {
     try {
       const res = await AuthService.register(data)
       return res.data
     } catch (error) {
+      return thunkAPI.rejectWithValue(error)
+    }
+  }
+)
+
+export const loginTC = createAsyncThunk<IAuthResponse, ILogin>('auth/login', async (data, thunkAPI) => {
+    try {
+      const res = await AuthService.login(data)
+      return res.data
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error)
+    }
+  }
+)
+
+export const logoutTC = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+    await AuthService.logout()
+  }
+)
+
+export const checkAuthTC = createAsyncThunk<IAuthResponse>('auth/check-auth', async (_, thunkAPI) => {
+    try {
+      const res = await AuthService.getNewTokens()
+      return res.data
+    } catch (error) {
+      if (errorCatch(error) === 'jwt expired') {
+        thunkAPI.dispatch(logoutTC())
+        throw new Error('Your authorization is finished, please sign in again')
+      }
+
       return thunkAPI.rejectWithValue(error)
     }
   }
